26080f58ee56aba0dc0d5c3c00ea3de255ec7d10
Gb-codertech <gbvieira2007@gmail.com>
2024-11-15 13:09:04 +0000

    Add files via upload

---
+++ DiferençaDeIdade/DiferençaDeIdade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class DiferençaDeIdade
+{
+    public static void Main(string[] args)
+    {
+        int idade = 64;
+        bool isSolteiro = false;
+
+        if (idade >= 18 && idade <= 65 && isSolteiro)
+        {
+            Console.WriteLine("És adulto e solteiro");
+        }
+        else if (idade >= 18 && idade <= 65 && !isSolteiro)
+        {
+            Console.WriteLine("És adulto e casado");
+        }
+        else if (idade >= 65)
+        {
+            Console.WriteLine("És sénior");
+        }
+        else
+        {
+            Console.WriteLine("És menor de idade");
+        }
+    }
+}
